db2e335b1fe6a5d2c9ad17e528fbc941039bf850
chauid <dlfdl28@naver.com>
2024-01-07 04:39:50 +0900

    Hook 기능 추가 및 에러 핸들링, No-image 추가

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,13 +1,18 @@
-export async function handle({ event, resolve }) {
-	if (event.url.pathname.startsWith('/admin')) {
-		return new Response('관리자 페이지', { status: 404 });
-	}
-	const response = await resolve(event);
-	return response;
-}
+import { type Handle, type HandleServerError } from '@sveltejs/kit';
 
-export async function handleError({ error, event, status, message }) {
-	return {
-		message: `msg: ${message}`,
-	};
-}
+export const handle: Handle = async ({ event, resolve }) => {
+  if (event.url.pathname.startsWith('/admin')) {
+    console.log('관리자 세션 로직 구현');
+    return new Response('관리자 페이지', { status: 200 });
+  }
+
+  const response = await resolve(event);
+  return response;
+};
+
+export const handleError: HandleServerError = async ({ event, status }) => {
+  return {
+    message: '페이지에 이상이 생겼습니다.',
+    code: `${status} + ${event.url.pathname}`,
+  };
+};
--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
-	import { page } from '$app/stores';
+  import { page } from '$app/stores';
 </script>
 
-<h1>Error</h1>
-<p>status: {$page.status}</p>
-<p>{$page.error?.message}</p>
+<div class="flex h-[100vh] w-[100w] flex-col flex-wrap items-center justify-center bg-gray-200">
+  <h1 class="text-3xl font-bold">Page Load Error</h1>
+  <p>status: {$page.status}</p>
+  <p>message: {$page.error?.message}</p>
+  <p>code: {$page.error?.code}</p>
+</div>
