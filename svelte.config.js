3754c2c17fd7ee2b4bc0d5ef2d7820ffd249d141
546d393061476c755a7942306279425461473933 <70273749+chauid@users.noreply.github.com>
2024-01-14 01:56:12 +0900

    프로젝트 설정 및 폰트 설정, DB 스키마 추가, favicon 설정

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,17 @@
 import adapter from '@sveltejs/adapter-netlify';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import path from 'path';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
     adapter: adapter(),
+    alias: {
+      $src: path.resolve('./src'),
+      $components: path.resolve('./src/components'),
+      $lib: path.resolve('./src/lib'),
+      $routes: path.resolve('/src/routes'),
+    },
   },
   preprocess: vitePreprocess(),
 };
