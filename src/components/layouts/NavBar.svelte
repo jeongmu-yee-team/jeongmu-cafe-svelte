1e3f3391aac5d5a782912d701cb5046c8eec7387
donismoney <jaehyun2112@naver.com>
2024-01-19 17:25:22 +0900

    store 전역 변수로 NavBar의 type_id 값 받아오기

--- src/components/layouts/NavBar.svelte
+++ src/components/layouts/NavBar.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import type { TProductType } from '$lib/server/modules/products';
   import { useSWR } from 'sswr';
+  import { selectedProductTypeId } from '$lib/store';
 
   interface IResponse {
     status: number;
@@ -9,6 +10,10 @@
   }
 
   const { data: navData, isLoading, error } = useSWR<IResponse>('/api/products-type');
+  function handleButtonClick(productTypeId: number) {
+    selectedProductTypeId.set(productTypeId);
+  }
+  $: console.log('selectedProductTypeId changed:', $selectedProductTypeId);
 </script>
 
 <nav>
@@ -16,7 +21,10 @@
     <div class="ml-2 mr-2 mt-2 flex flex-col items-center justify-between rounded-lg bg-yellow-200 py-3">
       <div class="ml-2 flex space-x-5">
         {#each $navData.productType as item}
-          <button class="mb-1 border-black text-xl font-bold duration-300 hover:scale-105 hover:border-b-4">
+          <button
+            on:click={() => handleButtonClick(item.id)}
+            class="mb-1 border-black text-xl font-bold duration-300 hover:scale-105 hover:border-b-4"
+          >
             {item.type_name}
           </button>
         {/each}
