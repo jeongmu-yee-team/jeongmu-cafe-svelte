d21426b2b07a781a74a15301ee66bbbeb7a0154b
donismoney <jaehyun2112@naver.com>
2024-01-19 17:33:33 +0900

    page div 영역 추가

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import IntroModal from '$components/modals/IntroModal.svelte';
   import NavBar from '$components/layouts/NavBar.svelte';
+  import Menu from '$components/layouts/Menu.svelte';
 
   let introModalToggle: boolean = false;
   function handleIntroModal() {
@@ -14,5 +15,10 @@
 
 <main class="font-cafe">
   <IntroModal toggleModal={handleIntroModal} modalStatus={introModalToggle} />
-  <NavBar />
+  <div class="bg-white">
+    <div class="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+      <NavBar />
+      <Menu />
+    </div>
+  </div>
 </main>
