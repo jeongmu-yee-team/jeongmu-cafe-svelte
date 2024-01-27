f3e3b44e59b7b7e00b2b9f64d790c88e4cb9c106
donismoney <jaehyun2112@naver.com>
2024-01-28 04:35:32 +0900

    네비바 다른  구역을 누르면 focus 효과가 풀리는 현상 수정

--- src/components/layouts/NavBar.svelte
+++ src/components/layouts/NavBar.svelte
@@ -2,33 +2,37 @@
   import type { TProductType } from '$lib/server/modules/products';
   import { useSWR } from 'sswr';
   import { selectedProductTypeId } from '$lib/store';
-
   interface IResponse {
     status: number;
     message: string;
     productType: TProductType;
   }
-
   const { data: navData, isLoading, error } = useSWR<IResponse>('/api/products-type');
+
   function handleButtonClick(productTypeId: number) {
     selectedProductTypeId.set(productTypeId);
   }
-  $: console.log('selectedProductTypeId changed:', $selectedProductTypeId);
 </script>
+
+<style>
+  .selected {
+    border-bottom: 4px solid black;
+    transform: scale(1.1);
+  }
+</style>
 
 <nav>
   {#if $navData}
-    <div class="ml-2 mr-2 mt-2 flex flex-col items-center justify-between rounded-lg bg-yellow-200 py-3">
-      <div class="ml-2 flex space-x-5">
-        {#each $navData.productType as item}
-          <button
-            on:click={() => handleButtonClick(item.id)}
-            class="mb-1 border-black text-xl font-bold duration-300 hover:scale-105 hover:border-b-4"
-          >
-            {item.type_name}
-          </button>
-        {/each}
-      </div>
+    <div class="ml-2 mr-2 mt-2 flex flex-row justify-around rounded-lg bg-yellow-200 py-3">
+      {#each $navData.productType as item}
+        <button
+          on:click={() => handleButtonClick(item.id)}
+          class="mb-1 border-black text-xl font-bold"
+          class:selected={$selectedProductTypeId === item.id}
+        >
+          {item.type_name}
+        </button>
+      {/each}
     </div>
   {/if}
 </nav>
