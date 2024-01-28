f4a19cdedc6b69a5f674a873010817f7e8a10384
donismoney <jaehyun2112@naver.com>
2024-01-28 16:54:16 +0900

    navbar 바인딩 tailwin 식으로 수정

--- src/components/layouts/NavBar.svelte
+++ src/components/layouts/NavBar.svelte
@@ -2,11 +2,13 @@
   import type { TProductType } from '$lib/server/modules/products';
   import { useSWR } from 'sswr';
   import { selectedProductTypeId } from '$lib/store';
+
   interface IResponse {
     status: number;
     message: string;
     productType: TProductType;
   }
+
   const { data: navData, isLoading, error } = useSWR<IResponse>('/api/products-type');
 
   function handleButtonClick(productTypeId: number) {
@@ -14,21 +16,14 @@
   }
 </script>
 
-<style>
-  .selected {
-    border-bottom: 4px solid black;
-    transform: scale(1.1);
-  }
-</style>
-
 <nav>
   {#if $navData}
     <div class="ml-2 mr-2 mt-2 flex flex-row justify-around rounded-lg bg-yellow-200 py-3">
       {#each $navData.productType as item}
         <button
           on:click={() => handleButtonClick(item.id)}
-          class="mb-1 border-black text-xl font-bold"
-          class:selected={$selectedProductTypeId === item.id}
+          class="mb-1 border-black text-xl font-bold
+          {`${$selectedProductTypeId === item.id ? 'scale-110 transform border-b-4 duration-200' : ''}`}"
         >
           {item.type_name}
         </button>
